4b056235802b92b23d8bc0c0aeb97898f29b155c
StrongSunshine <369532314@qq.com>
2020-04-21 23:56:22 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={require('./assets/img/logo.svg')} className="logo" alt="logo" />
+      <h1>连字游戏</h1>
+    </div>
+  );
+}
+
+export default App;
